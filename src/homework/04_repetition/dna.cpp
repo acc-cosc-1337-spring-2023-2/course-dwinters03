ceac953911da96054d0110bb0f824400a77d0650
dwinters03 <123131532+dwinters03@users.noreply.github.com>
2023-02-20 06:12:24 +0000

    Homework 4 Completed

--- src/homework/04_repetition/dna.cpp
+++ src/homework/04_repetition/dna.cpp
@@ -1,3 +1,44 @@
 //add include statements
+#include <iostream>
+#include "dna.h"
 
 //add function(s) code here
+
+int factorial(int num)
+{
+    int sum = 1;
+
+    /*while (num > 0)
+    {
+        sum = sum + (num*num);
+        num = num - 1;
+    }*/
+
+    for (int i = num; i >= 1; i--)
+    {
+        sum *= i;
+    }
+
+    return sum;
+}
+
+int gcd(int num1, int num2)
+{
+    while (num1 != num2)
+    {
+        if (num1 < num2)
+        {
+            int holdingnum;
+            holdingnum = num2;
+            num2 = num1;
+            num1 = holdingnum;
+        }
+
+        if (num1 > num2)
+        {
+            num1 = num1 - num2;
+        }
+    }
+
+    return num1;
+}
--- src/homework/04_repetition/main.cpp
+++ src/homework/04_repetition/main.cpp
@@ -1,7 +1,12 @@
 //write include statements
-
+#include <iostream>
+#include <string>
+#include "dna.h"
 //write using statements
 
+using std::cout;
+using std::cin;
+using std::string;
 
 /*
 Write code that prompts user to enter 1 for Get GC Content, 
@@ -12,5 +17,56 @@ user enters a y or Y.
 */
 int main() 
 {
+	int numresponse;
+
+	do
+	{
+		cout << "1 - Factorial\n2- Greatest Common Divisor\n3 - Exit\n";
+		cin >> numresponse;
+
+		switch(numresponse)
+		{
+			case 1:
+				int num;
+				cout << "Enter the number you would like to see the factorial for.\n";
+				cin >> num;
+				cout << factorial(num);
+				cout << "\n";
+				return 0;
+				break;
+
+			case 2:
+				int num1;
+				int num2;
+				cout << "Enter the two numbers you would like to see the GCD for.\nNumber 1:\n";
+				cin >> num1;
+				cout << "\nNumber 2:\n";
+				cin >> num2;
+				cout << "\n";
+				cout << gcd(num1, num2);
+				cout << "\n";
+				return 0;
+				break;
+
+			case 3:
+				char confirm;
+				cout << "Are you sure you want to quit? (Y, N)";
+				cin >> confirm;
+				if (confirm == 'Y')
+				{
+					cout << "Goodbye\n";
+				}
+				else
+				{
+					numresponse = 0;
+				}
+				break;
+
+			default:
+				cout << "Invalid Input";
+				break;
+		}
+	}	
+	while (numresponse != 3);
 	return 0;
 }
